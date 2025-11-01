0a01eac68c3be91ecdf1e1ff41efaa014aeb2c43
Andrés Darín <andres.darin05@gmail.com>
2025-11-01 16:59:15 -0300

    feat: update Hero component styling for improved responsiveness; modify package.json dev script for better accessibility

--- src/components/layout/hero/Hero.tsx
+++ src/components/layout/hero/Hero.tsx
@@ -6,7 +6,7 @@ import CTA from '@/components/ui/CTA';
 const Hero = () => {
     return (
         <>
-            <section className="relative w-full h-[900px] border-b border-black">
+            <section className="relative w-full h-screen md:h-[750px] lg:h-[900px] border-b border-black">
                 <Header />
                 <Image
                     src="/img/bg-hero-light.webp"
@@ -15,17 +15,19 @@ const Hero = () => {
                     style={{ objectFit: 'cover' }}
                     className="absolute inset-0"
                 />
-                <div className="relative z-10 flex flex-col items-start justify-end h-full pb-[12rem] pl-[4rem] text-left text-white">
-                    <h1 className="text-6xl font-thin">Stitchline</h1>
-                    <p className="mt-4 w-[30%] font-thin text-lg">
+                <div className="relative z-10 flex flex-col items-start justify-end h-screen pb-16 px-6 sm:pb-20 sm:px-8 md:pb-28 md:px-12 lg:pb-48 lg:pl-16 text-left text-white">
+                    <h1 className="text-4xl sm:text-5xl md:text-5xl lg:text-6xl font-thin">
+                        Stitchline
+                    </h1>
+                    <p className="mt-3 w-full sm:w-[70%] md:w-[60%] lg:w-[30%] font-thin text-sm sm:text-base md:text-lg leading-relaxed">
                         Discover timeless fashion crafted with care, designed to fit your lifestyle and express your individuality.
                     </p>
-                    <CTA href="Store" variant="primary" className="mt-6">Shop Now</CTA>
+                    <CTA href="Store" variant="primary" className="mt-6 sm:mt-7 md:mt-8">
+                        Shop Now
+                    </CTA>
                 </div>
-
             </section>
         </>
-
     );
 };
 
